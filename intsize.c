afe69dbf9ca6f2305783a290882ca07a0e263c15
tarunbodapati <36191528+tarunbodapati@users.noreply.github.com>
2019-06-28 18:06:38 +0530

    Create intsize.c

---
+++ intsize.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+   int i;
+   scanf("%d",&i);
+   if((i>=0)&&(i<10))
+   {
+   	printf("1");
+   }
+   else if((i>=100)&&(i<=999))
+   {
+   	printf("3");
+   }
+   else if((i>10)&&(i<=99))
+   {
+   	printf("2");
+   }
+   else
+   printf(">3");
+   return 0;
+}
