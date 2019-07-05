fc8cd597e2762b09c4f8cc9ff727a7898d29b42e
tarunbodapati <36191528+tarunbodapati@users.noreply.github.com>
2019-07-05 21:40:43 +0530

    Create betweenprimes.c

---
+++ betweenprimes.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int a,i,j,count=0,n;
+	scanf("%d %d",&a,&n);
+	i=a;
+   for(i=i+1;i<n;i++)
+   {
+   	for(j=1;j<n;j++)
+   	{
+   		if(i%j==0)
+   		{
+   			count++;
+  
+   			
+   	    }
+   	}
+   		if(count==2)
+   			{
+   			printf("%d\n",i);
+		   }
+		   count=0;
+	   
+   }
+   			
+   			
+	   
+   
+
+
+	return 0;
+}
