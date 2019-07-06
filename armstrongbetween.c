12b509160b69680fbefa48dccea68a0d7185970c
tarunbodapati <36191528+tarunbodapati@users.noreply.github.com>
2019-07-06 22:30:02 +0530

    Create armstrongbetween.c

---
+++ armstrongbetween.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,i,c,d,e=0,h;
+   scanf("%d %d",&a,&b);
+   i=a;
+   for(i=i+1;i<b;i++)
+   {
+   	h=i;
+   	while(h!=0)
+   	{
+   	
+   		c=h%10;
+   		h=h/10;
+   		d=c*c*c;
+   		e=e+d;
+	   }
+
+	   if(e==i)
+	   {
+	   	printf("%d \n",i);
+	   	
+	   }
+	   e=0;
+}
+	   
+ 
+   return 0;
+}
