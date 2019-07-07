cfca62530163c9c95424fecb83be108c2c8bc452
tarunbodapati <36191528+tarunbodapati@users.noreply.github.com>
2019-07-07 19:21:08 +0530

    Update multiples.c

--- multiples.c
+++ multiples.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-	int a,i;
-	scanf("%d",&a);
+	int b,i;
+	scanf("%d",&b);
 	for(i=1;i<=5;i++)
 	{
-		printf("%d\t",a*i);
+		printf("%d\t",b*i);
 		
 	}
 	return 0;
