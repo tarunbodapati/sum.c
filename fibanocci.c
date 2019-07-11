d116c7985f2c809aac55fb24facc00f6521978bd
tarunbodapati <36191528+tarunbodapati@users.noreply.github.com>
2019-07-11 19:44:30 +0530

    Create fibanocci.c

---
+++ fibanocci.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,x=1,i=1,j,c;
+	scanf("%d",&a);
+	
+	
+printf("%d %d",i,j);
+
+		while(j<a)
+		{
+		c=i+j;
+		i=j;
+		j=c;
+		printf(" %d",c);
+		
+	}
+	return 0;
+}
