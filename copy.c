d555c118dd423783bd6e374c1c1fa4b995ae8ff3
tarunbodapati <36191528+tarunbodapati@users.noreply.github.com>
2019-07-11 17:39:52 +0530

    Create copy.c

---
+++ copy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+ 
+
+void swap(int*, int *);
+ 
+void main()
+{
+    int num1, num2;
+
+    scanf("%d %d", &num1, &num2);
+    printf(" %d %d", num1, num2);
+    swap(&num1, &num2);       
+    printf(" %d %d", num1, num2);
+}
+ 
+
+void swap(int *a, int *b)
+{
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
+}
