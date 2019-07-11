191a079ff9a76e0557e5a34896aafe5623388dd7
tarunbodapati <36191528+tarunbodapati@users.noreply.github.com>
2019-07-11 16:54:44 +0530

    Create string2.c

---
+++ string2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  char a[100];
+  int i;
+  int count=0,x;
+  gets(a);
+  
+
+    for(i=0;a[i]!='\0';i++)
+    {
+       if(a[i] == ' ')
+	       count++;
+	   }
+              
+
+
+ 
+ printf("%d",count+1);
+	return 0;
+	
+}
