b2885e37b954533ba27eafae5a6bab556a6d200a
tarunbodapati <36191528+tarunbodapati@users.noreply.github.com>
2019-07-13 17:46:08 +0530

    Create 42pp.c

---
+++ 42pp.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char c[100],a[100];
+	int x=0,count=0,i,j;
+     scanf("%s %s",&c,&a);
+	for(i=0;c[i]!='\0';i++)
+	{
+		count++;
+	}
+	for(j=0;a[j]!='\0';j++)
+	{
+		x++;
+	}
+	if(count>x)
+	{
+		puts(c);
+	}
+	else
+	{
+		puts(a);
+	}
+	return 0;
+}
