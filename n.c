575353b1dd96738726b53efbf8e8f60caacf904d
tarunbodapati <36191528+tarunbodapati@users.noreply.github.com>
2019-07-15 18:57:34 +0530

    Update n.c

--- n.c
+++ n.c
@@ -1,21 +1,31 @@
 #include<stdio.h>
 int main()
 {
-	int d,c,i=3,a[10],j=2;
+	int d,c,i,a[10],j,count=0,x,z=0;
 	scanf("%d",&d);
+	x=d;
+	
+	while(x!=0)
+	{
+		x=x/10;
+		count++;
+
+	}
 	while(d!=0)
 	{
 		c=d%10;
-		j=i-1;
-		for(i=j;i>=0;i--)
+		
+		for(i=z;i<count;i++)
 		{
 			a[i]=c;
 			c=0;
 			break;
 		}
-		d=d/10;
+		
+		z++;
+		 d=d/10;
 	}
-for(i=0;i<=2;i++)
+for(i=count-1;i>=0;i--)
 {
 printf("%d ",a[i]);
 }
