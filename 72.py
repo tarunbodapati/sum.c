3ac31447e9629f5cdbb72ad95aebda7dd2e2f068
tarunbodapati <36191528+tarunbodapati@users.noreply.github.com>
2019-07-16 18:46:41 +0530

    Create 72.py

---
+++ 72.py
@@ -0,0 +1,14 @@
+x=list(input())
+count=0
+for k in x:
+    
+     if((k=='a')or(k=='e')or(k=='i')or(k=='o')or(k=='u')):
+         count=count+1
+       
+if(count>0):
+   print("yes")
+else:
+    print("no")
+ 
+
+
