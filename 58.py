4f6e005d4fcfb889d7dc3cd14a6089ba5f737124
tarunbodapati <36191528+tarunbodapati@users.noreply.github.com>
2019-07-16 11:12:29 +0530

    Create 58.py

---
+++ 58.py
@@ -0,0 +1,11 @@
+x,z = map(int,input().split())
+c = list(map(int,input().split()))
+count=0
+for j in c:
+    if (j==z):
+       print("yes")
+       break
+    else:
+        print("no")
+        break
+
